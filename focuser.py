b692148001496725dd9729036fdc367e1cf7c289
vinicius <bilck.vinicius1998@gmail.com>
2024-08-06 15:24:41 -0300

    Commands

--- focuser.py
+++ focuser.py
@@ -21,14 +21,26 @@ class FocuserController:
         self.speed = 0.193
         #self.goZero()
 
+    def forceZero(self):
+        if self.ser and self.ser.is_open:
+            print("Sent command to go to zero.")
+            self.moveIn()
+            time.sleep(20)
+            self.getStop()
+            print("Sent command to stop.")
+            self.new_time = 0.0
+            self.old_time = 0.0
+            self.mov_time = 0.0
+        
     def goZero(self):
         time.sleep(1)
         if self.ser and self.ser.is_open:
-            self.ser.write(b'L')
             print("Sent command to go to zero.")
-            time.sleep(30)  # Adjust this sleep duration if necessary
-            self.ser.write(b'S')
+            self.goTo(2.2)
             print("Sent command to stop.")
+            self.new_time = 0.0
+            self.old_time = 0.0
+            self.mov_time = 0.0
         else:
             print("Serial connection not open. Cannot send command.")
 
@@ -44,37 +56,45 @@ class FocuserController:
         for i in fsolve(f, [-20,20]):
             if i > 0:
                 self.new_time = i
-        print(f"D = {position} mm, Time = {self.new_time} s")
+        print(f"D = {position} mm")
 
-    def getPosition(self, _time):
-        self.position = _time * self.speed
-   
+    def getPosition(self):
+        f = lambda x: self.coef[0] - self.position + self.coef[1]*x + (self.coef[2]*x**2)/2
+        self.position = f(self.old_time)
+
+        self.position = self.old_time    
     def goTo(self, position):
         time.sleep(1)
-        self.getTime(position)
-        if self.new_time < self.old_time:
-            self.mov_time = self.old_time - self.new_time
-            self.old_time = self.new_time
-            self.moveOut()
-            time.sleep(self.mov_time)
-            self.getStop()
-        if self.old_time < self.new_time:
-            self.mov_time = self.new_time - self.old_time
-            self.old_time = self.new_time
-            self.moveIn()
-            time.sleep(self.mov_time)
-            self.getStop()
+        if (position >=  2.2) and (position <= 15):
+            self.getTime(position)
+            if self.new_time < self.old_time:
+                self.mov_time = self.old_time - self.new_time
+                print(f'time: {self.mov_time}')
+                self.old_time = self.new_time
+                self.moveIn()
+                time.sleep(self.mov_time)
+                self.getStop()
+            if self.old_time < self.new_time:
+                self.mov_time = self.new_time - self.old_time
+                print(f'time: {self.mov_time}')
+                self.old_time = self.new_time
+                self.moveOut()
+                time.sleep(self.mov_time)
+                self.getStop()
+        else:
+            print('Invalid position')
+
 
     def moveIn(self):
         if self.ser and self.ser.is_open:
-            self.ser.write(b'H')
+            self.ser.write(b'L')
             print("Sent command to move in.")
         else:
             print("Serial connection not open. Cannot send command.")
 
     def moveOut(self):
         if self.ser and self.ser.is_open:
-            self.ser.write(b'L')
+            self.ser.write(b'H')
             print("Sent command to move out.")
         else:
             print("Serial connection not open. Cannot send command.")
@@ -92,6 +112,7 @@ class FocuserController:
         if self.ser and self.ser.is_open:
             self.ser.close()
 
+"""
 if __name__ == "__main__":
     motor_controller = FocuserController('/dev/ttyUSB0')  # Update with your serial port
     if motor_controller.ser and motor_controller.ser.is_open:
@@ -104,4 +125,4 @@ if __name__ == "__main__":
         
     else:
         print("Failed to initialize the motor controller.")
-
+"""
